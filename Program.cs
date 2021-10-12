fe09ec0a32d664bb280418cd184dc89f2c426191
Natanael Lucena <nldm.eng20@uea.edu.br>
2021-10-12 11:10:08 -0400

    Create Console Application layout to start project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stock_quote_alert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
